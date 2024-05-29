ab7c3305cb9e1463b00182966158bad52e9daf7d
Almir Shala <as61238@ubt-uni.net>
2024-05-29 19:25:56 +0200

    Made Footer for the website

--- cinetixxfront/src/pages/Home.tsx
+++ cinetixxfront/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import MovieList from "../components/MovieList"
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
     <div>
    <Header/>
    <MovieList/>
+   <Footer/>
    </div>
   )
 }
