d81fcb6d76deb3e99bf297a90dd945ac61df1aa8
altin53 <120563289+altin53@users.noreply.github.com>
2024-06-01 11:23:00 +0300

    Update Header.tsx
    made the navbar responsive

--- cinetixxfront/src/components/Header.tsx
+++ cinetixxfront/src/components/Header.tsx
@@ -6,29 +6,34 @@ const Header = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+
+  };
+
+  // Function to close the menu
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+
   };
 
   return (
     <header className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className=" container mx-auto flex justify-between items-center text-lg">
         <div className="flex items-center">
-          <h1 className="text-2xl font-bold">Cinetixx</h1>
+          <h1 className="text-2xl font-bold ml-3">Cinetixx</h1>
         </div>
-        <nav className={`${isMenuOpen ? 'block' : 'hidden'} md:block md:flex flex-grow justify-center`}>
-          <ul className="flex space-x-4">
-            <li><a href="#" className="hover:text-gray-300">Home</a></li>
-            <li><a href="#" className="hover:text-gray-300">About</a></li>
-            <li><a href="#" className="hover:text-gray-300">Services</a></li>
-            <li><a href="#" className="hover:text-gray-300">Contact</a></li>
-          </ul>
-        </nav>
-        <div className="flex items-center space-x-4">
-          <button className="hidden md:block bg-gray-200 text-gray-800 px-4 py-2 rounded-lg"><Link to="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Sign in
-                            </Link></button>
-          <button className="hidden md:block bg-orange-200 text-gray-800 px-4 py-2 rounded-lg"><Link to="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Sign up
-                            </Link></button>
+        <nav className="hidden sm:hidden md:flex md:flex-grow md:justify-center md:font-bold sm:flex">
+  <ul className="flex  md:flex-row space-x-10">
+    <li className='relative left-10 md:left-0'><a href="#" className="hover:text-gray-300">Home</a></li>
+    <li><a href="#" className="hover:text-gray-300">About</a></li>
+    <li><a href="#" className="hover:text-gray-300">Services</a></li>
+    <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+  </ul>
+</nav>
+
+
+        <div className="flex items-center space-x-4 mr-3">
+          <button className="hidden md:block  bg-[#FFFFEC] text-gray-800 px-4 py-2 rounded"><Link to="/login" className="font-medium text-[#676c76] hover:text-indigo-200">Sign In</Link></button>
+          <button className="hidden md:block bg-[#DD761C] text-gray-800 px-4 py-2 rounded"><Link to="/register" className="font-medium text-white hover:text-indigo-200">Sign Up</Link></button>
           <button className="md:hidden" onClick={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               {isMenuOpen ? (
@@ -40,6 +45,26 @@ const Header = () => {
           </button>
         </div>
       </div>
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-black  flex justify-center items-center ">
+          <div>
+            <button className="absolute top-10 right-10 m-2" onClick={closeMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+            <ul className="text-center font-bold text-4xl mb-40 ">
+              <li className="m-10"><a href="#" className="hover:text-gray-300">Home</a></li>
+              <li className="m-10"><a href="#" className="hover:text-gray-300">About</a></li>
+              <li className="m-10"><a href="#" className="hover:text-gray-300">Services</a></li>
+              <li className="m-10"><a href="#" className="hover:text-gray-300">Contact</a></li>
+              
+            </ul>
+            <button className="relative right-10 bottom-20 w-40 bg-[#FFFFEC] text-gray-800 px-4 py-2 rounded"><Link to="/login" className="font-medium text-[#676c76] hover:text-indigo-200">Sign In</Link></button>
+          <button className=" relative left-10 bottom-20 w-40 bg-[#DD761C] text-gray-800 px-4 py-2 rounded"><Link to="/register" className="font-medium text-white hover:text-indigo-200">Sign Up</Link></button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
